382e50efe8a1afa02c3e3bd6bfba167257fc5a80
raagh <rk00968@surrey.ac.uk>
2024-05-24 00:09:22 +0100

    hashmaps added

--- src/Hashmapss.java
+++ src/Hashmapss.java
@@ -19,5 +19,49 @@ public class Hashmapss {
             }
         };
 
-    }
+        List<String> I = new ArrayList<String>(){
+            {
+                add("Iceland");
+                add("India");
+                add("Indonesia");
+                add("Iran");
+                add("Iraq");
+                add("Ireland");
+        }
+
+    };
+
+            List<String> U = new ArrayList<String>(){
+                {
+                    add("Uganda");
+                    add("Ukraine");
+                    add("United Arab Emirates");
+                    add("United Kingdom");
+                    add("United States");
+                    add("Uruguay");
+                    add("Uzbekistan");
+                }
+            };
+
+            // initialise hashmap
+            countries.put("G", G);
+            countries.put("I", I);
+            countries.put("U", U);
+
+            // get list of countries starting with I
+            List<String> result = countries.get("I");
+            for(String country : result){
+                System.out.println(country);
+            }
+
+            //remove key/value
+            countries.remove("I");
+            System.out.println();
+
+            //try to get I
+            System.out.println("Try to get value after deleting");
+            System.out.println(countries.get("I"));
+        }
+
+
 }
